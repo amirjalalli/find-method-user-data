fd7a10a5dec160ff4a69efc04ea5ac60826a22a7
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-12-15 10:14:10 +0330

    forEach-parctis-again

---
+++ js.js
@@ -0,0 +1,11 @@
+let userData = [
+    {id:1 , username:'ali' , password:'13123jsdf'},
+    {id:2 , username:'amir' , password:'sdfasdf'},
+    {id:3 , username:'hasan' , password:'13gggg'},
+    {id:4 , username:'mmd' , password:'23323'},
+    ]
+
+
+userData.forEach(function(user){
+    console.log('username'+':'+user.username  ,'password'+':'+user.password)
+})
